3ef55b976ddac600e0d07cdc4cba74a603a5c8e2
Oliver <olihomuth1@gmx.de>
2025-11-21 11:07:29 +0100

    02 / Instanziierungen und Variablen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,11 +4,26 @@ public class App {
     
     public static void main(String[] args) {
 
-        System.out.println(new Cat());
+        // System.out.println(new Cat());
 
+        // Instanziierung
+        // cat = Referenzvariable
+        // Cat = Referenztyp
+
+        Cat cat = new Cat();
+        output("Blick von aussen: " + cat);
+        output("Blick von Innen: " + cat.getInstanceVariable());
+
+        // cat.tellYourAdress();
     }
 
-   
+
+    // statische Methode
+    public static void output(String outputStr){
+        System.out.println(outputStr);
+    }
+
+
 
 }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,18 @@ package com.cc.java;
 
 public class Cat {
 
-    
+    public void sayHello(){
+        System.out.println("Hello!");
+    }
+
+
+    // this --> Instanzvariable
+    public void tellYourAdress(){
+        App.output("Blick von Innen " + this);
+    }
+
+    public Cat getInstanceVariable(){
+        return this;
+    }
 
 }
